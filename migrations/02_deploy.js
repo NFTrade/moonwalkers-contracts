03715522292665ddd139473a1ede27f1b6d3636e
Shahaf Antwarg <santwarg@gmail.com>
2022-01-14 23:46:30 +0200

    sample

--- migrations/02_deploy.js
+++ migrations/02_deploy.js
@@ -1,7 +1,7 @@
 const MoonWalkers = artifacts.require('MoonWalkers');
 
 const deploy = async (deployer, network, accounts) => {
-  await deployer.deploy(MoonWalkers, 'Moon Walkers', 'MNWLKRS', "https://ipfs.nftrade.com/ipfs/QmX9Uyub6tJriV6MK24A3brkJVchxXqHBWBBXpm86s4AQK/1073.json");
+  await deployer.deploy(MoonWalkers, 'Moon Walkers', 'MNWLKRS', "https://ipfs.io/ipfs/example");
 };
 
 module.exports = deploy;
